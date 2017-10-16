f4b2849576c49040fd41a64df673ea6dca333d7b
roctbb <roctbb@gmail.com>
2017-10-16 16:40:19 +0300

    Добавлены примеры microsoft

--- 5 - Microsoft Cognitive/1 - faces.py
+++ 5 - Microsoft Cognitive/1 - faces.py
@@ -1,5 +1,8 @@
+from PIL import Image
+
 # импортируем модуль для запросов в интернет
 import requests
+
 
 # это адрес сервиса для распознавания эмоций
 url = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize"
@@ -21,6 +24,11 @@ result = requests.post(url, headers=header, data=image)
 # парсим json и на выходе получаем словарь с лицами
 faces = result.json()
 
+print(faces)
+
+big_image = Image.open("people.jpg")
+
+
 # в цикле выводим координаты каждого лица
 for face in faces:
     print('--- '*10)
@@ -28,3 +36,31 @@ for face in faces:
     print("y - ", face['faceRectangle']['top'])
     print("w - ", face['faceRectangle']['width'])
     print("h - ", face['faceRectangle']['height'])
+
+    x = face['faceRectangle']['left']
+    y = face['faceRectangle']['top']
+    width = face['faceRectangle']['width']
+    height = face['faceRectangle']['height']
+
+    emotions = face['scores']
+    print(emotions)
+
+    max_emotion = 'anger'
+    max_value = 0
+
+    for emotion in emotions:
+        if emotions[emotion] > max_value:
+            max_value = emotions[emotion]
+            max_emotion = emotion
+
+    print(max_emotion, " - ", max_value)
+
+    if max_emotion == "neutral":
+        smile = Image.open(max_emotion+".png").convert("RGBA")
+    else:
+        smile = Image.open("face.png")
+
+    smile = smile.resize((width, height))
+    big_image.paste(smile, (x,y), smile)
+
+big_image.show()
